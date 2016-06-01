5563bc15f79cc12ebd3babdedc523e7dc70299fc
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2016-06-01 13:18:49 +0000

    BinTest1

---
+++ test/binaryNetTest.R
@@ -0,0 +1,81 @@
+
+## Network with two proficiency variables and observables for each
+## different type of rule
+
+binAll <- CreateNetwork("binAll")
+PnetPriorWeight(binAll) <- 11           #Give it something to see.
+
+## Set up Proficiency Model.
+thetas <- NewDiscreteNode(binAll,paste("theta",0:1,sep=""),
+                          c("Low","Med","High"))
+names(thetas) <- paste("theta",0:1,sep="")
+NodeParents(thetas[[2]]) <- thetas[1]
+
+for (nd in thetas) {
+  NodeLevels(nd) <- effectiveThetas(NodeNumStates(nd))
+  PnodeRules(nd) <- "Compensatory"
+  PnodeLink(nd) <- "normalLink"
+  PnodeBetas(nd) <- 0
+  NodeSets(nd) <- c("pnodes","Proficiency")
+}
+
+## Standard normal prior.
+PnodeAlphas(thetas[[1]]) <- numeric()
+PnodeLinkScale(thetas[[1]]) <- 1
+## Regression with a correlation of .6
+PnodeAlphas(thetas[[2]]) <- .6
+PnodeLinkScale(thetas[[2]]) <- .8
+
+### Now Create Observables
+obsnames <- c("t0","t1","comp","conj","disj","oconj","odis")
+obs <- NewDiscreteNode(binAll,obsnames)
+names(obs) <- obsnames
+
+for (nd in obs) {
+  NodeParents(nd) <- thetas
+  NodeLevels(nd) <- c(1,0)
+  PnodeLink(nd) <- "partialCredit"
+  NodeSets(nd) <- c("pnodes","onodes","Observables")
+}
+
+## Anchor nodes which tap only single skills.
+NodeParents(obs$t0) <- list(thetas$theta0)
+NodeParents(obs$t1) <- list(thetas$theta1)
+PnodePriorWeight(obs$t0) <- 25
+PnodePriorWeight(obs$t1) <- 25
+
+## Leave conjuctive model at defaults
+
+## For conjunctive model make Theta 0 more important
+PnodeRules(obs$conj) <- "Conjunctive"
+PnodeLnAlphas(obs$conj) <- c(theta0=0,theta1=-.5)
+PnodeBetas(obs$conj) <- 1              #Slightly hard
+
+PnodeRules(obs$oconj) <- "OffsetConjunctive"
+PnodeLnAlphas(obs$oconj) <- 0
+PnodeBetas(obs$oconj) <- c(theta0=1,theta1=-1)
+
+## Reverse parents of disjunctive models, to make sure we
+## Get numbers in correct places.
+NodeParents(obs$disj) <- rev(thetas)
+PnodeRules(obs$disj) <- "Disjunctive"
+PnodeLnAlphas(obs$disj) <- c(theta1=.5,theta0=-.5)
+PnodeBetas(obs$disj) <- 0
+
+NodeParents(obs$odis) <- rev(thetas)
+PnodeRules(obs$odis) <- "OffsetDisjunctive"
+PnodeLnAlphas(obs$odis) <- 0
+PnodeBetas(obs$odis) <- c(theta1=.5,theta1=-.5)
+
+binAll.Q <- Pnet2Qmat(binAll,obs,thetas,defaultBeta=0)
+write.csv(binAll.Q,"binall.Q.csv",na="",row.names=FALSE)
+
+binAll.Omega <- Pnet2Omega(binall,thetas)
+write.csv(binAll.Omega,"binall.Omega.csv",na="",row.names=FALSE)
+
+
+
+
+
+
+
