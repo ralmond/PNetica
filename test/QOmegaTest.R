d283c96621b3437588f323c40ff88488a36624f5
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2018-01-25 02:27:02 +0000

    Summary: Put stuff in.

--- test/QOmegaTest.R
+++ test/QOmegaTest.R
@@ -1,5 +1,10 @@
 sess <- NeticaSession()
 startSession(sess)
+curd <- getwd()
+## Insures we are building nets from scratch
+setwd(tempdir())
+
+
 netman1 <- read.csv(paste(library(help="PNetica")$path, "auxdata",
                           "Mini-PP-Nets.csv", sep=.Platform$file.sep),
                     row.names=1,stringsAsFactors=FALSE)
@@ -11,6 +16,22 @@ nodeman1 <- read.csv(paste(library(help="PNetica")$path, "auxdata",
 omegamat <- read.csv(paste(library(help="PNetica")$path, "auxdata",
                            "miniPP-omega.csv", sep=.Platform$file.sep),
                      row.names=1,stringsAsFactors=FALSE)
-## profnames <- omegamat$Node
-## rownames(omegamat) <- profnames
+Q1 <- read.csv(paste(library(help="PNetica")$path, "auxdata",
+                           "miniPP-Q.csv", sep=.Platform$file.sep),
+                     stringsAsFactors=FALSE)
+
+Nethouse <- BNWarehouse(manifest=netman1,session=sess,key="Name")
+
+Nodehouse <- NNWarehouse(manifest=nodeman1,
+                         key=c("Model","NodeName"),
+                         session=sess)
+
+
+## Test building with the Omega matrix.  Start by making a blank net.
+CM <- WarehouseSupply(Nethouse,"miniPP_CM")
+CM1 <- Omega2Pnet(omegamat,CM,Nodehouse,debug=TRUE)
+
+
+
 stopSession(sess)
+setwd(curd)
