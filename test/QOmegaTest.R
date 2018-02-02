b93ea72e990aafdd77bbed10430706de2a0b8821
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2018-02-02 02:56:33 +0000

    Singapore

--- test/QOmegaTest.R
+++ test/QOmegaTest.R
@@ -40,5 +40,22 @@ stopifnot(all.equal(Om2,omegamat))
 
 Qmat2Pnet(Q1, Nethouse,Nodehouse,debug=TRUE)
 
+## Hard code the model list so always in the same order.
+obs <-sapply(list(sess$nets$PPcompEM,sess$nets$PPconjEM,sess$nets$PPtwostepEM),
+             NetworkAllNodes)
+
+Q2 <- Pnet2Qmat(obs,NetworkAllNodes(CM),debug=TRUE)
+
+## Fix columns marked as logical because they were blank.
+Q1$LinkScale <- as.numeric(Q1$LinkScale)
+Q1$A.Physics <- as.numeric(Q1$A.Physics)
+Q1$A.IterativeD <- as.numeric(Q1$A.IterativeD)
+Q1$B.Physics <- as.numeric(Q1$B.Physics)
+Q1$B.NTL <- as.numeric(Q1$B.NTL)
+Q1$PriorWeight <- rep("",nrow(Q1)) #Coverted to logical on read.
+
+all.equal(Q1[,-1],Q2)
+
+
 stopSession(sess)
 setwd(curd)
