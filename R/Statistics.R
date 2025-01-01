cddf99e7cddd3cfdc5482a57214777661b1756a6
Russell G Almond <russell.g.almond@gmail.com>
2024-12-31 20:50:24 -0500

    Changed signature of PnodeMargin to (node,net=NULL) from (net,node). Moved "character" methods to `Peanut` from `PNetica.`

--- R/Statistics.R
+++ R/Statistics.R
@@ -83,54 +83,19 @@
 ##'
 NULL
 
-setMethod("PnodeMargin",c("NeticaBN","NeticaNode"),
-          function(net,node) NodeBeliefs(node))
-setMethod("PnodeMargin",c("NeticaBN","character"),
-          function(net,node) {
-            nd <- NetworkFindNode(net,node)
-            if (!is.NeticaNode(nd))
-              stop("Can't find node named ",node)
-            PnodeMargin(net,nd)
-          })
-setMethod("PnodeEAP",c("NeticaBN","NeticaNode"),
-          function(net,node) c(NodeExpectedValue(node))) #clear SD attribute c()
-setMethod("PnodeEAP",c("NeticaBN","character"),
-          function(net,node) {
-            nd <- NetworkFindNode(net,node)
-            if (!is.NeticaNode(nd))
-              stop("Can't find node named ",node)
-            PnodeEAP(net,nd)
-          })
-setMethod("PnodeSD",c("NeticaBN","NeticaNode"),
-          function(net,node) {
+setMethod("PnodeMargin",c("NeticaNode"),
+          function(node,net=NULL) NodeBeliefs(node))
+setMethod("PnodeEAP",c("NeticaNode"),
+          function(node,net=NULL) c(NodeExpectedValue(node))) #clear SD attribute c()
+setMethod("PnodeSD",c("NeticaNode"),
+          function(node,net=NULL) {
             ev <- NodeExpectedValue(node)
             if (is.na(ev)) return(NA)
             attr(ev,"std_dev")
             })
-setMethod("PnodeSD",c("NeticaBN","character"),
-          function(net,node) {
-            nd <- NetworkFindNode(net,node)
-            if (!is.NeticaNode(nd))
-              stop("Can't find node named ",node)
-            PnodeSD(net,nd)
-          })
-setMethod("PnodeMedian",c("NeticaBN","NeticaNode"),
-          function(net,node)
+setMethod("PnodeMedian",c("NeticaNode"),
+          function(node,net=NULL)
             names(which(cumsum(NodeBeliefs(node))>=.5))[1])
-setMethod("PnodeMedian",c("NeticaBN","character"),
-          function(net,node) {
-            nd <- NetworkFindNode(net,node)
-            if (!is.NeticaNode(nd))
-              stop("Can't find node named ",node)
-            PnodeMedian(net,nd)
-          })
-setMethod("PnodeMode",c("NeticaBN","NeticaNode"),
-          function(net,node)
+setMethod("PnodeMode",c("NeticaNode"),
+          function(node,net=NULL)
             names(which.max(NodeBeliefs(node))))
-setMethod("PnodeMode",c("NeticaBN","character"),
-          function(net,node) {
-            nd <- NetworkFindNode(net,node)
-            if (!is.NeticaNode(nd))
-              stop("Can't find node named ",node)
-            PnodeMode(net,nd)
-          })
